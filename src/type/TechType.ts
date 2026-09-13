b24966418505d5cf54e083678753c5990d1dcb3a
tortoisejr <mdsabbirh083@gmail.com>
2026-09-13 10:25:29 +0600

    create type for Technology

---
+++ src/type/TechType.ts
@@ -0,0 +1,10 @@
+export interface Itech {
+  id: string;
+  name: string;
+  category: string;
+  description: string;
+  icon: string;
+  rating: number;
+  difficulty: string;
+  badge: string;
+}
